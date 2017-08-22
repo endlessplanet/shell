ecfde480fbf7bcb6185dd64c79ca07e31dbe97c5
Emory Merryman <emory.merryman@gmail.com>
2017-08-22 00:49:47 +0000

    installing git and openssh

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sleep infinity
+echo "${ID_RSA}" > /home/user/.ssh/id_rsa &&
+    chmod 0600 /home/user/.ssh/id_rsa &&
+    chown user:user /home/user/.ssh/id_rsa &&
+    sleep infinity
--- image/root/run.sh
+++ image/root/run.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
 dnf update --assumeyes &&
+    dnf install --assumeyes git openssh-clients meld &&
     adduser user &&
     mkdir /workspace &&
     chown user:user /workspace &&
+    mkdir /home/user/.ssh &&
+    chmod 0700 /home/user/.ssh &&
+    chown user:user /home/user/.ssh &&
     dnf update --assumeyes &&
     dnf clean all

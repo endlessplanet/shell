43a292f95b60e9c347036bf070337bd4f18c9a43
Emory Merryman <emory.merryman@gmail.com>
2017-08-22 01:35:58 +0000

    init git

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -2,5 +2,10 @@
 
 echo "${ID_RSA}" > /home/user/.ssh/id_rsa &&
     chmod 0600 /home/user/.ssh/id_rsa &&
-    chown user:user /home/user/.ssh/id_rsa &&
+    cd "/workspace/${PROJECT_NAME}" &&
+    git init &&
+    git config user.name "${USERNAME}" &&
+    git config user.email "${EMAIL}" &&
+    git remote add origin "${URL}" &&
+    cp /opt/docker/post-commit.sh .git/hooks/post-commit &&
     sleep infinity

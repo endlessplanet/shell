ad37dd94d15a88655244d6992786c65d6bb223e3
Emory Merryman <emory.merryman@gmail.com>
2017-08-23 12:16:47 +0000

    it works

--- image/root/run.sh
+++ image/root/run.sh
@@ -3,9 +3,6 @@
 dnf update --assumeyes &&
     dnf install --assumeyes git openssh-clients meld &&
     adduser user &&
-    mkdir /workspace &&
-    chown user:user /workspace &&
-    mkdir /home/user/.ssh &&
     chmod 0700 /home/user/.ssh &&
     chown user:user /home/user/.ssh &&
     dnf update --assumeyes &&

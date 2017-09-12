6921028aaa7c2bda943db737d53615be1b31b1f9
Emory Merryman <emory.merryman@gmail.com>
2017-09-12 10:12:52 +0000

    static tagging and remove logging

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
-    docker image build --tag endlessplanet/shell:$(git rev-parse --verify HEAD) image &&
-    docker image push endlessplanet/shell:$(git rev-parse --verify HEAD)
+TAG=endlessplanet/shell:$(git rev-parse --verify HEAD) &&
+    docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
+    docker image build --tag ${TAG} image &&
+    docker image push ${TAG}

bdad096432bbf2bb952c07fabdbd8ad5464a686c
Emory Merryman <emory.merryman@gmail.com>
2017-09-16 11:37:55 +0000

    remove a bunch of stuff and change ownership of adventure dir

--- image/root/entrypoint.sh
+++ image/root/entrypoint.sh
@@ -1,25 +1,5 @@
 #!/bin/sh
 
-echo "${ID_RSA}" > ${HOME}/.ssh/id_rsa &&
-    echo "${KNOWN_HOSTS}" > ${HOME}/.ssh/known_hosts &&
-    chmod 0600 ${HOME}/.ssh/id_rsa ${HOME}/.ssh/known_hosts &&
-    mkdir /workspace/${PROJECT_NAME} &&
-    cd "/workspace/${PROJECT_NAME}" &&
-    git init &&
-    git config user.name "${USERNAME}" &&
-    git config user.email "${EMAIL}" &&
-    if [ ! -z "${ORIGIN}" ]
-    then
-        git remote add origin "${ORIGIN}"
-    fi &&
-    if [ ! -z "${REPORT}" ]
-    then
-        git remote add report "${REPORT}"
-    fi &&
-    if [ ! -z "${UPSTREAM}" ]
-    then
-        git remote add upstream "${UPSTREAM}" &&
-            git remote set-url --push upstream no_push
-    fi &&
-    ln --symbolic --force ${HOME}/bin/post-commit .git/hooks/post-commit &&
+sudo chown --recursive user:user /workspace/$(ls -1 /workspace)
+    cd "/workspace/$(ls -1 /workspace)" &&
     sleep infinity

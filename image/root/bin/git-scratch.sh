b110d49f662e19b2735417c3e280a4e3195c8d0a
Emory Merryman <emory.merryman@gmail.com>
2017-09-17 00:51:18 +0000

    fix scratch script and add HOME/bin to PATH

--- image/root/bin/git-scratch.sh
+++ image/root/bin/git-scratch.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
 MASTER_BRANCH="${1}" &&
+    git diff --exit-code &&
+    git diff --cached --exit-code &&
+    [ -z "$(git ls-files --other --exclude-standard --directory)" ] &&
     git fetch upstream "${MASTER_BRANCH}" &&
+    git checkout "upstream/${MASTER_BRANCH}" &&
     git checkout -b scratch/$(uuidgen)

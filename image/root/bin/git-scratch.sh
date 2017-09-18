9db82a0cee268139e61c3aa0f24f8d5588d232fe
Emory Merryman <emory.merryman@gmail.com>
2017-09-18 13:48:27 +0000

    wtf

--- image/root/bin/git-scratch.sh
+++ image/root/bin/git-scratch.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
 MASTER_BRANCH="${1}" &&
-    git diff --exit-code &&
-    git diff --cached --exit-code &&
-    [ -z "$(git ls-files --other --exclude-standard --directory)" ] &&
     git fetch upstream "${MASTER_BRANCH}" &&
-    git checkout "upstream/${MASTER_BRANCH}" &&
     git checkout -b scratch/$(uuidgen)
--- image/root/shell.sh
+++ image/root/shell.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 cd /workspace/$(ls -1 /workspace) &&
-    export PATH=${HOME}/bin:${PATH} &&
     bash
